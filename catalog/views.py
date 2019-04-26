aca353fc619ad2f03eded4451dd2150c360dc129
Angel Gomez <jangelgr7@gmail.com>
2019-04-26 14:37:26 -0400

    Add can_mark_returned permission and use it in borrowed books
    The view which list all borrowed books require the permission
    can_mark_returned.

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
 from catalog.models import Book, Author, BookInstance, Genre
 
@@ -59,3 +59,14 @@ class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
 
     def get_queryset(self):
         return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+
+
+class BorrowedBooksAllListView(PermissionRequiredMixin, generic.ListView):
+    """Generic class-based view listing books on loan."""
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_all.html'
+    paginate_by = 10
+    permission_required = 'catalog.can_mark_returned'
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(status__exact='o').order_by('due_back')
